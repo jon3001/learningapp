2536b5f9b40367ab4d0f820b34e366cd2f67bd98
Jonathan Lonsdale <jon@jlonsdale.net>
2024-12-08 21:20:26 +0000

    Attempt to fix project file for builds via GitHub

--- obj/Debug/net9.0/learningapp.AssemblyInfo.cs
+++ obj/Debug/net9.0/learningapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fbdc0736bcb62079bfe285f95fec35e47d1fa2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
